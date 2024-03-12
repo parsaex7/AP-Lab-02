33bd0a4c5dd073e12ad00c16fcd60852af78839e
parsaex7 <parsaexir2005@gmail.com>
2024-03-12 12:40:06 +0330

    Make student class and modify main

--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        Student std1 = new Student("James", "Gosling", "0987654");
+        Student std2 = new Student("Dennis", "Ritchie", "123467");
+        std1.SetGrade(18);
+        std2.SetGrade(17.5);
+        std1.PrintStudentInfo();
+        std2.PrintStudentInfo();
+    }
 }
+
+
--- Student.java
+++ Student.java
@@ -1,2 +1,36 @@
 public class Student {
+    private String Name;
+    private String LastName;
+    private String Id;
+    private double Grade;
+
+    public Student(String name, String lastName, String id) {
+        if (id.length() != 7) // check if id length equals to 7
+            System.out.println("Invalid ID");
+        else {
+            Name = name;
+            LastName = lastName;
+            Id = id;
+            Grade = 0;
+        }
+    }
+
+    public void SetGrade(double grade) {
+        if (grade > 20 || grade < 0) //check whether grade is valid or no
+            System.out.println("Invalid Grade");
+        else
+            Grade = grade;
+    }
+
+    public double GetGrade() {
+        return Grade;
+    }
+
+    public void PrintStudentInfo() {
+        if (Name == null || LastName == null || Id == null) // if id is not equals to 7
+        {
+            System.out.println("Invalid Data");
+        } else
+            System.out.println(Name + " " + LastName + "\nID : " + Id + "\nGrade : " + Grade);
+    }
 }
